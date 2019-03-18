63407d6092e5c6febf913bdf1382df3948a4bf51
AaronHKim <46009098+AaronHKim@users.noreply.github.com>
2019-03-17 17:48:58 -0700

    fixed by pushing jekyll
    got equations and spacing to work

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,6 +1,11 @@
 var store = [{
-        "title": "Post 2",
-        "excerpt":"Post Post Post Heading These are subheadings This is my second post! YAY Bisection Method INTRODUCTION A recursive root finding algorithm that takes in two values and finds the a midpoint between them. The next iterate always chooses two values with different signs. If the root exists and sign of...","categories": [],
+        "title": "False Position",
+        "excerpt":"False Position Method INTRODUCTION We would like a recursive algorithm that does the following: start with point slope formula: solve for fixed points by setting: Here, we need an initial guess for the left and right bounds: a,b We then update the formula using a similar procedure as in bisection...","categories": [],
         "tags": ["test","post"],
-        "url": "http://localhost:4000/test/",
+        "url": "http://localhost:4000/false-position/",
+        "teaser":null},{
+        "title": "Bisection Method",
+        "excerpt":"Bisection Method INTRODUCTION A recursive root finding algorithm that takes in two values and finds the a midpoint between them. The next iterate always chooses two values with different signs. If the root exists and sign of f(left) and f(right) are different (one is a negative number and the other...","categories": [],
+        "tags": ["test","post"],
+        "url": "http://localhost:4000/bisection/",
         "teaser":null}]
